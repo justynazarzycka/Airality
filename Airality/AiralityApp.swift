605240865f45660dab81bc99b3b295c12549511c
Justyna Zarzycka <justyna.zarzycka70@gmail.com>
2021-03-15 09:07:44 +0100

    Initial Commit

---
+++ Airality/AiralityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiralityApp.swift
+//  Airality
+//
+//  Created by Justyna Zarzycka on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AiralityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airality/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Airality
+//
+//  Created by Justyna Zarzycka on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
